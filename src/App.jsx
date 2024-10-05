a0ab591b4318d045f0af28e8d5804465d399cbdd
mustaine611 <arboleda.paulo2001@gmail.com>
2024-10-05 12:06:56 -0500

    Añadiendo header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header/Header";
+
+
+
+function App() {
+  return (
+    <div>
+        <Header/>
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+export const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Este es el encabezado de la app</h1>
+            <p>Esto es un texto generico</p>
+        </div>
+
+    )
+
+}
