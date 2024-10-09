66d87e583b66b9c6a0acb01c780f24943c8448d4
mustaine611 <arboleda.paulo2001@gmail.com>
2024-10-08 23:18:58 -0500

    agregando boton buscar artistas

--- src/Api/Pspotify.jsx
+++ src/Api/Pspotify.jsx
@@ -1,4 +1,4 @@
- 
+import { useEffect, useState } from 'react';
 
 var obj = {
     method: 'POST',
@@ -12,18 +12,51 @@ var obj = {
             'client_secret' : 'ea52bde724294073bf9361b824382904'
         }
     )
-
-
 }
 
 
+export class Cancion{
+  constructor( nombre ,duration_ms, id    ){
+    this.nombre = nombre
+    this.duration_ms=duration_ms
+    this.id     = id 
+  }
+}
 
-export  const Pspotify = () => {
+export function getSounds ( token  ){ 
+  const market = "ES"; 
+  const limit = 10;     
+  const offset = 5;  
+
+  const idAlbum = "1zrVeZHRsPsRWdnQSuUIYG"
+  return fetch(`https://api.spotify.com/v1/albums/${idAlbum}/tracks`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`, 
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.items); 
+    return data.items
+  })
+  .catch(error => {
+    console.error("Error fetching tracks:", error);
+  });
+}
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export const Pspotify = () => {
     const  token = fetch("https://accounts.spotify.com/api/token", obj)
         .then((response => {
             return response.json()
         }))
-        .then( json => {console.log(json)})
+        .then( json => {
+          console.log(json.access_token) 
+          return  json.access_token
+        })
         .catch(error => console.error(error))
     return token 
     
--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,52 @@ import './Api/Api'
 import { Header } from "./Header/Header";
 import { Navbar } from "./Navbar/Navbar";
 import Api from './Api/Api';
-import { Pspotify } from './Api/Pspotify';
-
+import { Pspotify ,Cancion ,getSounds} from './Api/Pspotify';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  var [nombre, setNombre] = useState("");
+  var [data, setDatas] = useState([]);
+
+  const getAlbumNach = async () => {
+    try {
+      let token = await Pspotify(); 
+      console.log(token);
+      let listData = await getSounds(token);
+      
+      setDatas(listData.map(a => new Cancion(a.name, a.duration_ms, a.track_number)));
+      
+    } catch (error) {
+      console.error("Error al obtener el token o sonidos:", error);
+    }
+  }
+  var  filter  = data.filter( (a) => a.nombre.toLowerCase().includes( nombre.toLowerCase() ) )
+  const manejarCambio = (event ) =>{
+    setNombre(event.target.value)
+
+  }
 
 
 
   return (
     <div className='app'>
       
+      <input 
+        type="text" 
+        plaseholder="buscar nombres"
+        value={nombre}
+        onChange= {manejarCambio}
+        
+      />
+     <ul>
+      {    
+        filter.length > 0 ?( filter.map( (a) => <li>  numero :  {a.id},  nombre : {a.nombre} ,  duracion :  {a.duration_ms} </li> ) )
+        : ( filter.map( (a) => <li>  numero :  {a.id},  nombre : {a.nombre} ,  duracion :  {a.duration_ms} </li> ) )
+         }
       
-      <button onClick={Pspotify()}>update list </button>
+     </ul>
+      <button onClick={() => getAlbumNach()}>update list</button>
 
     </div>
     
